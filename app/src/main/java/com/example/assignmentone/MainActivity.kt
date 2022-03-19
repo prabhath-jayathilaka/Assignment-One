2fbad7a3035411a85cdfe641e46431fcf7877011
codeJay <90417125+prabhath-jayathilaka@users.noreply.github.com>
2022-03-19 22:35:27 +0530

    enter key event added

--- app/src/main/java/com/example/assignmentone/MainActivity.kt
+++ app/src/main/java/com/example/assignmentone/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.assignmentone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import androidx.lifecycle.Observer
+import android.view.KeyEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
 import com.example.assignmentone.databinding.ActivityMainBinding
 import retrofit2.Response
@@ -15,33 +18,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view =binding.root
+        val view = binding.root
         setContentView(view)
-binding.btnSearch.setOnClickListener{
-
-
-        val retService =RetrofitInstance.getRetrofitInstance().create(PostAPI::class.java)
-        val responseLiveData : LiveData<Response<Post>> = liveData {
-            val response: Response<Post> =retService.getPost(binding.etPostNumber.text.toString().toInt())
-            emit(response)
+        binding.btnSearch.setOnClickListener {
+            searchPost()
         }
-        responseLiveData.observe(this, Observer {
-            val  post =it.body()
-          if (post !=null){
-              Log.i("API",post.title)
-              binding.tvId.text = post.id.toString()
-              binding.tvUserId.text = post.userId.toString()
-              binding.tvTitle.text = post.title
-              binding.tvBody.text = post.body
-          }
+
+        binding.etPostNumber.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                searchPost()
+                return@OnKeyListener true
+            }
+            false
         })
 
 
-
-
-
-
-
-
     }
-}}
+
+    @SuppressLint("SetTextI18n")
+    private fun searchPost() {
+
+        val retService = RetrofitInstance.getRetrofitInstance().create(PostAPI::class.java)
+        val responseLiveData: LiveData<Response<Post>> = liveData {
+            val response: Response<Post> =
+                retService.getPost(binding.etPostNumber.text.toString().toInt())
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val post = it.body()
+            if (post != null) {
+                Log.i("API", post.title)
+                binding.tvId.text = "Past Id :" + post.id.toString()
+                binding.tvUserId.text = "User Id :" + post.userId.toString()
+                binding.tvTitle.text = "Title :" + post.title
+                binding.tvBody.text = "Body :" + post.body
+            }
+        })
+    }
+}
